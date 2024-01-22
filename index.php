9b6cbf64b2af2fce8f4f7c90ad32d629d1a68dfd
FRFalk <146141132+FRFalk@users.noreply.github.com>
2024-01-22 14:03:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<?php
-if (isset($_REQUEST["p"])) {
-    $pagina = $_REQUEST["p"];
-} else {
-    $pagina = ""; //
-}
-
-include "/templates/general.php";
-?>
-    <head>
-        <link rel="stylesheet" href="/stylesheet/bootstrap1.css">
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5rFOJ6y5R+RfJuYn2M+HXKx3vLFmXY7Xa5XnHhczl7V2Crh" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-o+RDVZABeJzgA1f2JKT/CrZ6mG0M0dZz1sEJ1oA5l7dKJvqPJwo99X6FJ2RjzKt4" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyZ/X1OQlgRdC2BIBjN7jz7ls+X5p7kzoI" crossorigin="anonymous"></script>
-<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous">
-    </head>
-    <?php
-if(empty($pagina)) {
+<DOCTYPE html>
     <body>
         <h1>
         jiububb
@@ -24,6 +7,4 @@ if(empty($pagina)) {
          mnbibu
         </h2>
     </body>
-        <?php
-        }
-?>
+
